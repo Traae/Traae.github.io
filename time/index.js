be47a8648a5120c75ea33185e557a56c09ec705f
Traae <traaebloxham@gmail.com>
2024-08-22 15:41:59 -0600

    Updating the time tool demo

--- time/index.js
+++ time/index.js
@@ -15,66 +15,46 @@ https://github.com/jamezmca/calc
 //   const timeDisplay = ;
 //   displayInput.innerText = timeDisplay.trim() ? timeDisplay : "error";
 // }
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
 
 function startTime() {
   var time = new Date();
   var clock = time.toLocaleTimeString("en-US", { hour12: true });
 
   var h = time.getHours();
-  if (h < 10) {
-    h = "0" + h;
-  }
-  var m = Math.floor((time.getMinutes() / 60) * 100);
+  var m = time.getMinutes();
 
-  // add a zero in front of numbers<10
-
-  document.getElementById("uspsclock").innerHTML = h + "" + m;
+  document.getElementById("uspsclock").innerHTML = convertToPostalTime(h, m);
   document.getElementById("normalclock").innerHTML = clock;
+
   t = setTimeout(function () {
     startTime();
   }, 500);
 }
 startTime();
 
-function convertToPostalTime() {}
+function convertToPostalTime(hour, minute) {
+  var h = formatNumber(hour);
 
-/*
-// Date code
-const now = new Date();
+  var m = Math.floor((minute / 60) * 100);
+  m = formatNumber(m);
 
-const hours = now.getHours();
-const minutes = now.getMinutes();
-console.log(`Today is day ${day} and the time is ${hours}:${minutes}.`);
-
-const time = new Date();
-var hour = time.getHours();
-var minute = time.getMinutes();
-
-
-// timed refresh code
-function timedRefresh(timeoutPeriod) {
-  var timer = setInterval(function () {
-    if (timeoutPeriod > 0) {
-      timeoutPeriod -= 1;
-      document.body.innerHTML = timeoutPeriod + ".." + "<br />";
-      // or
-      document.getElementById("countdown").innerHTML =
-        timeoutPeriod + ".." + "<br />";
-    } else {
-      clearInterval(timer);
-      window.location.href = window.location.href;
-    }
-  }, 1000);
+  return h + m;
 }
-timedRefresh(10);
+function formatNumber(n) {
+  if (n < 10) {
+    return "0" + n;
+  }
+  return "" + n;
+}
 
-*/
+function convertToPostalButton() {
+  var input = document.getElementById("timeInput");
+  var output = convertToPostalTime(
+    parseInt(input.value.slice(0, 2)),
+    parseInt(input.value.slice(3, 5))
+  );
+  document.getElementById("convertToPostal").innerHTML = output;
+}
 
 /*
 const displayInput = document.getElementById("inputValue");
